63d3a4cd30cfa7d8fac87d88fb9feb0ed15ed832
masinwu <emwu83@gmail.com>
2018-11-24 12:28:56 +0000

    Basic addition.

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded',function(){
     btnCheck = document.querySelector('#btn_check');
     
     
-    
+
     // Some basic strings methods in JS
     // name = "Marcin";
     // surname = "Wo";
@@ -25,12 +25,12 @@ document.addEventListener('DOMContentLoaded',function(){
     // });
 
     // Some basic numbers in JS
-    let number_a, number_b, number_c,number_d, num_out;
+    // let number_a, number_b, number_c,number_d, num_out;
 
-    number_a = 40;
-    number_b = 35;
-    number_c = -10;
-    number_d = 1.4;
+    // number_a = 40;
+    // number_b = 35;
+    // number_c = -10;
+    // number_d = 1.4;
 
     // num_out = number_a + number_b;
     // num_out = Math.ceil(number_d);
@@ -39,16 +39,47 @@ document.addEventListener('DOMContentLoaded',function(){
     // num_out = Math.max(2345,234,5234,5623,53,2345,234,5234,6,4367,3456,2,623,6,46,243,6);
 
 
-    btnCheck.addEventListener('click', function(){
 
-        h1tag.innerHTML = num_out;
-        console.log(2=='2');
-        console.log(2==='2');
-        console.log(typeof(null));
-        console.log(typeof(undefined));
+    // btnCheck.addEventListener('click', function(){
+
+    //     h1tag.innerHTML = num_out;
+    //     console.log(2=='2');
+    //     console.log(2==='2');
+    //     console.log(typeof(null));
+    //     console.log(typeof(undefined));
 
         
+    // });
+
+
+    // let age_array = [23,43,5,23,12,45,58];
+
+
+
+    // let numA = '10', numB = 10;
+    // let me_person = {
+    //     name: 'Marcin',
+    //     age: '40'
+    // };
+    // num_out = Number(numA)+numB;
+    // num_out = numA.to+numB;
+    // num_out = age_array;
+    // num_out = me_person.name;
+
+    // btnCheck.addEventListener('click', function(){
+
+    //     h1tag.innerHTML = num_out;
+    //     console.log(num_out);
+    //     console.log(typeof num_out);
+        
+    // });
+
+    btnCheck.addEventListener('click', function(){
+
+        h1tag.innerHTML = 'Woo';
+        
     });
+
 
     
 });
